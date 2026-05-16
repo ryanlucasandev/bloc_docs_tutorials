f17ca856989ff95853cddf68822ceeea9ca784e4
ryanlucasandev <ryan.lucasan.dev@gmail.com>
2026-05-16 14:29:34 +0800

    Flutter Counter tutorial

---
+++ lib/bloc_tutorials/flutter_counter/app.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:bloc_docs_tutorials/bloc_tutorials/flutter_counter/counter/counter.dart';
+
+class CounterApp extends MaterialApp {
+  const CounterApp({super.key}) : super(home: const CounterPage());
+}
